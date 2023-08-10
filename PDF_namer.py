790a0a5dbc4ba1ca355629c9b26a9b275d190b4a
Devon <ds.pydev@gmail.com>
2023-08-10 12:11:16 +0200

    Add Decryption of PDF

--- PDF_namer.py
+++ PDF_namer.py
@@ -1,17 +1,37 @@
 from PyPDF2 import PdfReader
+from pypdf import PdfReader, PdfWriter
+from decrypt import password
 
-reader = PdfReader('Statement Unlocked.pdf')
-number_of_pages = len(reader.pages)
-page = reader.pages[0]
-text = page.extract_text()
+# Decrypt PDF
+reader = PdfReader("Locked.pdf")
+writer = PdfWriter()
 
-x = text.find('Statement period')
-# print(text[1302:1343])
+if reader.is_encrypted:
+    reader.decrypt(password)
 
-q = text[1302:1343]
-# print(q)
+# Add all pages to the writer
+for page in reader.pages:
+    writer.add_page(page)
 
-w = q[18:28] + ' - ' + q[31:41]
-e = str(int(w[0:2]) + 1) 
-r = e + w[2:]
-print(r)
+# Save the new PDF to a file
+with open("decrypted-pdf.pdf", "wb") as f:
+    writer.write(f)
+
+
+
+# # Look up Date of pdf and rename
+# reader = PdfReader('Statement Unlocked.pdf')
+# number_of_pages = len(reader.pages)
+# page = reader.pages[0]
+# text = page.extract_text()
+
+# x = text.find('Statement period')
+# # print(text[1302:1343])
+
+# q = text[1302:1343]
+# # print(q)
+
+# w = q[18:28] + ' - ' + q[31:41]
+# e = str(int(w[0:2]) + 1) 
+# r = e + w[2:]
+# print(r)
